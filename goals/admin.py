e9147cd081b8e823c57de4f51f6d7b7570ac71d5
vkarpovich <karpovich.valera@mail.ru>
2023-04-19 15:41:08 +0300

    lesson 36 finished version fix

--- goals/admin.py
+++ goals/admin.py
@@ -1,28 +1,27 @@
-
 from django.contrib import admin
 
 from goals.models import GoalCategory, Goal, GoalComment
 
 
-@admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'is_deleted', )
-    list_display_links = ('title', )
-    search_fields = ('title', )
-    list_filter = ('is_deleted', )
-    readonly_fields = ('created', 'updated',)
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user")
 
 
-@admin.register(Goal)
+admin.site.register(GoalCategory, GoalCategoryAdmin)
+
+
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'category', 'status', 'priority')
-    list_display_links = ('title',)
+    list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'description')
-    list_filter = ('status', 'priority')
-    readonly_fields = ('created', 'updated',)
 
 
-@admin.register(GoalComment)
+admin.site.register(Goal, GoalAdmin)
+
+
 class GoalCommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'text',)
     readonly_fields = ('created', 'updated',)
+
+
+admin.site.register(GoalComment, GoalCommentAdmin)
